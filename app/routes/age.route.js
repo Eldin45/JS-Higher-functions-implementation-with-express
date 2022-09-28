6b2b7e664a08120c92d6a6f00fbc6859ddb2f2f4
eldin45 <nmibrahim19@gmail.com>
2022-09-28 08:40:37 +0100

    updating age route

--- app/routes/age.route.js
+++ app/routes/age.route.js
@@ -3,7 +3,7 @@ import { addAge, getAges } from "../controllers/ages.controller.js";
 
 export default app=>{
     const route = express.Router();
-  
+  //route for adding age and also for retrieving fro your database
    route.post("/", addAge);
    route.get("/", getAges)
 
